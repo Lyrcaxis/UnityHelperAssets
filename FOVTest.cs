6b5ea30669723c4f0533df672adf67e4505998b9
Lyrc <32474602+Lyrcaxis@users.noreply.github.com>
2019-11-14 06:33:59 +0200

    Update FOVTest.cs

--- FOVTest.cs
+++ FOVTest.cs
@@ -7,10 +7,11 @@ public class FOVTest : MonoBehaviour {
 	public float AngleX = 45;
 	public float AngleY = 25;
 
-	[Space, Header("Debug Visual Stuff")]
-	public int Divisions = 50;
-	public int PlaneDistance = 10;
+	[Space, Header("Debug & Visuals")]
 	public bool DrawFOV = true;
+	public bool AlsoCheckOutside = false;
+	public int Divisions = 25;
+	public int PlaneDistance = 10;
 
 	void Awake() {
 		DrawFOV = false;
@@ -50,17 +51,20 @@ public class FOVTest : MonoBehaviour {
 		var planeHeight = PlaneDistance * 2 * Mathf.Tan(AngleY * Mathf.Deg2Rad);
 		var planeWidth = planeHeight * AngleX / AngleY;
 
+		var iterationMulti = 0.5f;
+		if (AlsoCheckOutside) { iterationMulti *= 4; }
+
 		// Divide the plane and draw rays towards it
-		for (float x = -planeWidth / 2; x < planeWidth / 2; x += planeWidth / Divisions) {
-			for (float y = -planeHeight / 2; y < planeHeight / 2; y += planeHeight / Divisions) {
+		for (float x = -planeWidth * iterationMulti; x < planeWidth * iterationMulti; x += planeWidth / Divisions) {
+			for (float y = -planeHeight* iterationMulti; y < planeHeight* iterationMulti; y += planeHeight / Divisions) {
 				var positionOnPlane = planeCenter;
 				positionOnPlane += x * transform.right;
 				positionOnPlane += y * transform.up;
 
 				// Make up for floating point errors
 				positionOnPlane += 0.000001f * transform.forward;
-        
-				Gizmos.color = IsWithinScope(positionOnPlane) ? new Color(0, 1, 0, 0.5f) : new Color(1, 0, 0, 0.01f);
+
+				Gizmos.color = IsWithinScope(positionOnPlane) ? new Color(0, 1, 0, 0.1f) : new Color(1, 0, 0, 0.005f);
 				Gizmos.DrawLine(transform.position, positionOnPlane);
 			}
 		}
