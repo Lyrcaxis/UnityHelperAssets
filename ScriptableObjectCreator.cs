5962c375bcc730de70004a053a3a6f4f77ab33a6
Lyrc <32474602+Lyrcaxis@users.noreply.github.com>
2019-11-25 22:22:13 +0200

    Create ScriptableObjectCreator.cs
    A ScriptableObject that can be used to design & create ScriptableObjects of any type with ease.

---
+++ ScriptableObjectCreator.cs
@@ -0,0 +1,112 @@
+#if UNITY_EDITOR
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace Utilities {
+
+	public abstract class ScriptableObjectCreator<T> : SerializedScriptableObject where T : ScriptableObject, new() {
+
+		protected abstract string SavePath { get; }
+		protected abstract void AdditionalInitialization(T obj);
+		protected abstract bool AdditionalValidation();
+		protected abstract void AdditionalReset();
+
+		[ShowInInspector, PropertyOrder(0)]
+		public string Name { get; set; }
+
+		T _item;
+		[ShowInInspector, PropertyOrder(1), InlineEditor, HideReferenceObjectPicker]
+		public T item {
+			get => _item;
+			set { }
+		}
+
+		void OnEnable() {
+			AdditionalReset();
+			_item = CreateInstance<T>();
+			_item.name = "New";
+			Name = "";
+		}
+
+
+		[Button, PropertyOrder(99)]
+		void CreateAsset() {
+
+			if (!AdditionalValidation()) { return; }
+			string path = "";
+
+			if (!SavePath.StartsWith("Assets/")) { path += "Assets/"; }
+
+			path += SavePath;
+			if (!SavePath.EndsWith("/")) { path += "/"; }
+			path += Name;
+			path += ".asset";
+
+			Debug.Log($"Create {Name} at: {path}...");
+			if (!ValidatePath(path)) { return; }
+
+			var obj = item;
+			AdditionalInitialization(obj);
+
+			AssetDatabase.CreateAsset(obj, path);
+			EditorUtility.SetDirty(obj);
+
+			Debug.Log($"{Name} successfully created as {path}");
+
+			LastCreatedAsset = obj;
+			OnEnable();
+
+		}
+
+		[SerializeField, HideInInspector] T LastCreatedAsset;
+
+		[Button, PropertyOrder(100)]
+		void GoToLastCreatedAsset() {
+			if (LastCreatedAsset != null) {
+				ProjectWindowUtil.ShowCreatedAsset(LastCreatedAsset);
+			}
+			else { Debug.Log("Last created asset cannot be found."); }
+		}
+
+		#region Path validation Logic
+		bool ValidatePath(string path) {
+			if (String.IsNullOrWhiteSpace(SavePath) || !IsStringValid(SavePath, Path.GetInvalidPathChars())) {
+				Debug.Log("Wrong Save Path");
+				return false;
+			}
+			if (String.IsNullOrWhiteSpace(Name) || !IsStringValid(Name, Path.GetInvalidFileNameChars())) {
+				Debug.Log("Wrong Name");
+				return false;
+			}
+			if (!AssetDatabase.IsValidFolder(SavePath)) {
+				Debug.Log("Wrong Folder");
+				return false;
+			}
+			if (UnityEngine.Windows.File.Exists(path)) {
+				Debug.Log("File already exists");
+				return false;
+			}
+
+			return true;
+		}
+
+		bool IsStringValid(string s, char[] invalidCharacters) {
+			foreach (var character in invalidCharacters) {
+				if (s.Contains(character.ToString())) {
+					Debug.Log(character);
+					return false;
+				}
+			}
+
+			if (s.Contains(".")) { return false; }
+
+			return true;
+		}
+		#endregion
+	}
+
+}
+#endif
