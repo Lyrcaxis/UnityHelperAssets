88deda8ca5fb91d11e44419d6b0e2fb40a246e54
Lyrc <32474602+Lyrcaxis@users.noreply.github.com>
2020-01-28 10:17:15 +0200

    No more errors :)
    The property is now only refreshing after the Repaint session.
    Added an Attribute for property identification. 
    
    It's possible to implement pathing back to parent, but it's not as useful as one may think and is prone to issues.
    I'll look into implementing injection for list elements after some time.

--- InjectPropertyAttribute.cs
+++ InjectPropertyAttribute.cs
@@ -1,10 +1,9 @@
 using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using System.Collections.Generic;
 #if UNITY_EDITOR
-using Sirenix.OdinInspector.Editor;
 using UnityEditor;
+using Sirenix.OdinInspector.Editor;
 #endif
 
 /// <summary>
@@ -16,152 +15,103 @@ public class InjectPropertyAttribute : ShowInInspectorAttribute {
 	public string MemberPath { get; private set; }
 	public bool DrawAboveTarget { get; private set; }
 
-#if UNITY_EDITOR
-	public InspectorProperty parentProperty { get; private set; }
-	public bool IsInitialized { get; private set; }
-
-	public void SetMemberPath(string value) => MemberPath = value;
-	public void SetParentProperty(InspectorProperty value) => parentProperty = value;
-	public bool MarkAsInitialized() => IsInitialized = true;
-#endif
-
 	/// <summary>
 	/// Injects the property to be drawn before or after the target member.
 	/// </summary>
-	/// <param name="MemberPath">Path of the member to be injected towards.</param>
+	/// <param name="MemberPath">Path of the member to be injected on.</param>
 	/// <param name="DrawAboveTarget">Should the property be drawn above the target member?</param>
 	public InjectPropertyAttribute(string MemberPath, bool DrawAboveTarget = true) {
 		this.MemberPath = MemberPath;
 		this.DrawAboveTarget = DrawAboveTarget;
 	}
+
 }
 
 #if UNITY_EDITOR
 namespace Sirenix.OdinInspector.CustomProcessors {
 
-	// Helper attribute to be attached to the target of InjectPropertyAttribute.
-	class InjectHelperAttribute : Attribute {
-		public InspectorProperty targetProperty;
-
-		public bool DrawTargetPropertyFirst;
-
-		public InjectHelperAttribute(InspectorProperty target, bool DrawTargetFirst) {
-			targetProperty = target;
-			DrawTargetPropertyFirst = DrawTargetFirst;
-		}
-	}
-
-	// Hey, if it works, it works :)
-	[ResolverPriority(-10000)]
-	class InjectorHack : OdinPropertyProcessor {
-		public static List<InjectorHackHelper> ToProcessList = new List<InjectorHackHelper>(100);
-
-		public override void ProcessMemberProperties(List<InspectorPropertyInfo> propertyInfos) {
-			foreach (InspectorPropertyInfo pInfo in propertyInfos) {
-				foreach (InjectorHackHelper item in ToProcessList) {
-					if (item.info.GetMemberInfo() != pInfo.GetMemberInfo()) { continue; }
-					pInfo.GetEditableAttributesList().Add(item.attr);
-					ToProcessList.Remove(item);
-					break;
-				}
-			}
-		}
-		internal class InjectorHackHelper {
-			public InspectorPropertyInfo info;
-			public InjectHelperAttribute attr;
-		}
-	}
-
-	// Draws the original drawer on the specified member of the InjectPropertyAttribute.
-	[DrawerPriority(super: 1000)]
-	class InjectedPropertyDrawer : OdinAttributeDrawer<InjectHelperAttribute> {
+	// Draws according to the original drawer on the specified member of the InjectPropertyAttribute.
+	[DrawerPriority(super: 999)]
+	class InjectedPropertyDrawer : OdinAttributeDrawer<InjectDrawerAttribute> {
 
 		protected override void DrawPropertyLayout(GUIContent label) {
 			if (!Attribute.DrawTargetPropertyFirst) { CallNextDrawer(label); }
 			DrawInjectedProperty();
 			if (Attribute.DrawTargetPropertyFirst) { CallNextDrawer(label); }
+
 		}
 
 		void DrawInjectedProperty() {
 			InjectDrawer.ShouldDraw = true;
-			{
-				//InjectDrawer.Draw(Attribute.targetProperty);
-				try { Attribute.targetProperty.Draw(); }
-				catch { Debug.LogError("Error drawing property."); }
-			}
+			Attribute.targetProperty.Draw();
 			InjectDrawer.ShouldDraw = false;
 		}
 	}
 
 	// The original drawer of the InjectPropertyAttribute.
 	// Skips drawing initially and can drawn on demand with ShouldDraw.
-	[DrawerPriority(super: 999)]
-	class InjectDrawer : OdinAttributeDrawer<InjectPropertyAttribute> {
+	[DrawerPriority(super: 1000)]
+	public class InjectDrawer : OdinAttributeDrawer<InjectPropertyAttribute> {
 		public static bool ShouldDraw;
-		public static bool SkipFrame;
+		bool hasValidPath;
 
-		// Validating here because it's impossible to scan down the hierarchy from Property or Attribute Processors.
 		protected override void Initialize() {
-			string result = Validate(Property, Attribute);
-			if (result != "Success") { Debug.LogError(result); }
+			if (Property.IsInitialized()) { return; }
+
+			string[] pathMembers = Attribute.MemberPath.Split('.');
+
+			InspectorProperty targetP = Property.Parent;
+			if (targetP == null) { targetP = Property.SerializationRoot; }
+
+			foreach (string nextPath in pathMembers) {
+				targetP = targetP.Children.Get(nextPath);
+				if (targetP == null) { return; }
+			}
+
+			Property.AddAttribute(new InitializedAttribute());
+			Property.UpdateOnNextRepaint();
+			targetP.AddAttribute(new InjectDrawerAttribute(Property, Attribute.DrawAboveTarget));
+			targetP.UpdateOnNextRepaint();
+
+			hasValidPath = true;
 		}
 
 		protected override void DrawPropertyLayout(GUIContent label) {
-			if (!Attribute.IsInitialized) {
-				EditorGUILayout.HelpBox($"{Attribute.MemberPath} is not a valid path.", MessageType.Error);
+			if (!Property.IsInitialized()) {
+				if (!hasValidPath) {
+					try { EditorGUILayout.HelpBox($"{Attribute.MemberPath} is not a valid path.", MessageType.Error); }
+					catch { Debug.LogWarning($"{Attribute.MemberPath} is not a valid path."); } // Unity was sometimes throwing an exception here so why not.
+					CallNextDrawer(label);
+				}
+				else if (ShouldDraw) { Debug.LogError("Was asked to draw even when property is not initialized."); }
+			}
+			else if (ShouldDraw) {
+				Property.Update(true);
 				CallNextDrawer(label);
 			}
-			else if (SkipFrame) { SkipFrame = false; }
-			else if (ShouldDraw) { CallNextDrawer(label); }
 		}
 
-		#region Validation
-		string Validate(InspectorProperty Property, InjectPropertyAttribute attr) {
-			if (!Property.IsReachableFromRoot()) { Debug.LogError($"{Property.Name} is not valid target for {attr}"); }
-
-			string[] hiearchyMembers = GetFullHierarchyPath(Property, attr).Split('.');
-
-			bool parentInitialized = false;
-			InspectorProperty p = Property.SerializationRoot;
-
-			foreach (string member in hiearchyMembers) {
-
-				if (!parentInitialized) { if (IsParent(Property, p)) { attr.SetParentProperty(p); parentInitialized = true; } }
-
-				InspectorProperty nextProp = p.Children.Get(member);
-				if (nextProp != null) { p = nextProp; }
-				else { return $"Could not find {member} in {p.Path}."; }
-			}
-
-			if (!parentInitialized) { return $"Parent not found for {Property.Name}."; }
-
-			var newAttr = new InjectHelperAttribute(Property, attr.DrawAboveTarget);
-			InjectorHack.ToProcessList.Add(new InjectorHack.InjectorHackHelper() { info = p.Info, attr = newAttr });
-			p.Parent.RefreshSetup();
-
-			attr.MarkAsInitialized();
-			SkipFrame = true;
-			return "Success";
-		}
-
-
-		string GetFullHierarchyPath(InspectorProperty Property, InjectPropertyAttribute attr) {
-			string path = attr.MemberPath.TrimStart('@');
-			if (Property.Path.Contains(".")) {
-				string subStr = Property.Path.Substring(0, Property.Path.LastIndexOf('.'));
-				path = subStr + "." + path;
-			}
-			return path;
-		}
-
-		bool IsParent(InspectorProperty Property, InspectorProperty CheckIfParent) {
-			foreach (InspectorProperty item in CheckIfParent.Children) {
-				if (item.Path == Property.Path) { return true; }
-			}
-			return false;
-		}
-		#endregion
 	}
 
+	// Helper attribute to be attached to the target of InjectPropertyAttribute.
+	class InjectDrawerAttribute : Attribute {
+		public InspectorProperty targetProperty;
+		public bool DrawTargetPropertyFirst;
+
+		public InjectDrawerAttribute(InspectorProperty target, bool DrawTargetFirst) {
+			targetProperty = target;
+			DrawTargetPropertyFirst = DrawTargetFirst;
+		}
+	}
+
+	// Attribute that marks a property as initialized.
+	class InitializedAttribute : Attribute { }
+
+	static class InspectorPropertyExtensions {
+		public static bool IsInitialized(this InspectorProperty p) => p.Attributes.Any(x => x is InitializedAttribute);
+		public static void AddAttribute(this InspectorProperty p, Attribute attribute) => p.Info.GetEditableAttributesList().Add(attribute);
+		public static void UpdateOnNextRepaint(this InspectorProperty p) => p.Tree.DelayActionUntilRepaint(p.RefreshSetup);
+	}
 }
+
 #endif
